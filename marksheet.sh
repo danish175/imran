93de40a0611847ef5ed3ae335d8df909190c38d1
danish175 <aryanarchitects17@gmail.com>
2021-03-18 14:45:21 +0530

    second commit

---
+++ marksheet.sh
@@ -0,0 +1,8 @@
+echo " enter your percent"
+read a
+if [ $a -gt 70 ]
+then
+echo " you are eligible to get addmission"
+else
+echo " you are not qualified"
+fi
---
+++ new.sh
@@ -0,0 +1,10 @@
+echo " enter any number"
+read a
+if [ $a -gt 0 ]
+then
+echo "$a is greater than 0"
+else 
+echo "$a is smaller than 0"
+fi
+
+
